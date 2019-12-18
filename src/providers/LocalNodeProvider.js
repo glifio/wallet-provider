a391b8084c84bca94e1f67584226e03aae03f960
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2019-12-17 19:43:22 -0500

    remove hardcoded requests

--- src/providers/LocalNodeProvider.js
+++ src/providers/LocalNodeProvider.js
@@ -7,21 +7,6 @@ class LotusRpcEngine {
   }
 
   async request(method, ...params) {
-    console.log(this.apiAddress,
-      {
-        jsonrpc: '2.0',
-        method: `Filecoin.${method}`,
-        params: [...params],
-        id: 1,
-      },
-      {
-        headers: {
-          'Content-Type': 'text/plain;charset=UTF-8',
-          Accept: '*/*',
-          Authorization: `Bearer ${this.token}`,
-        },
-      }
-    )
     const { data } = await axios.post(
       this.apiAddress,
       {
@@ -49,17 +34,9 @@ class LocalNodeProvider {
     this.jsonRpcEngine = new LotusRpcEngine({ apiAddress, token });
   }
 
-  // newAccount = () => this.jsonRpcEngine.request('WalletNew', 'secp256k1');
+  newAccount = () => this.jsonRpcEngine.request('WalletNew', 'secp256k1');
 
-  // getAccounts = () => this.jsonRpcEngine.request('WalletList');
-
-  newAccount = () => 't1jdlfl73voaiblrvn2yfivvn5ifucwwv5f26nfza';
-
-  getAccounts = () => [
-    't1tvg3q5ugbhykritgedk52btevvplutkdfmqjklq',
-    't1yqtd4xnqoptphyw6gfcvphkfs2dkthzfop4r4wa',
-    't1z225tguggx4onbauimqvxzutopzdr2m4s6z6wgi',
-  ];
+  getAccounts = () => this.jsonRpcEngine.request('WalletList');
 }
 
 export default LocalNodeProvider
