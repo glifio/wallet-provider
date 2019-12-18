d9abea42552abcc8a7ab703879aab1a58b96eb55
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2019-12-18 08:40:22 -0500

    use BigNumbers for balance

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import BigNumber from 'bignumber.js'
+
 import LotusRpcEngine from './LotusRPCEngine';
 
 export { default as LocalNodeProvider } from './providers/LocalNodeProvider'
@@ -10,7 +12,10 @@ class Filecoin {
     });
   }
 
-  getBalance = (address) => this.jsonRpcEngine.request('WalletBalance', address)
+  getBalance = async (address) => {
+    const balance = await this.jsonRpcEngine.request('WalletBalance', address)
+    return new BigNumber(balance)
+  }
 
   getNonce = () => {}
 
