1cbbaa4db71c962ed644133a7cf0db34a6ce7d16
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-01-18 13:37:51 -0500

    gracefully handle ledger busy errors

--- src/providers/LedgerProvider.js
+++ src/providers/LedgerProvider.js
@@ -5,6 +5,7 @@ class LedgerProvider extends FilecoinApp {
   constructor(transport) {
     super(transport)
     this.type = 'LEDGER'
+    this.ledgerBusy = false
   }
 
   handleErrors = response => {
@@ -27,11 +28,21 @@ class LedgerProvider extends FilecoinApp {
     throw new Error(response.error_message)
   }
 
+  throwIfBusy = () => {
+    if (this.ledgerBusy)
+      throw new Error(
+        'Ledger is busy, please check device or unplug and replug it in.',
+      )
+  }
+
   /* getVersion call rejects if it takes too long to respond,
   meaning the Ledger device is locked */
-  getVersion = () =>
-    new Promise((resolve, reject) => {
+  getVersion = () => {
+    this.throwIfBusy()
+    this.ledgerBusy = true
+    return new Promise((resolve, reject) => {
       setTimeout(() => {
+        this.ledgerBusy = false
         return reject(new Error('Ledger device locked or busy'))
       }, 3000)
 
@@ -41,31 +52,48 @@ class LedgerProvider extends FilecoinApp {
           return resolve(response)
         } catch (err) {
           return reject(err)
+        } finally {
+          this.ledgerBusy = false
         }
       })
     })
+  }
 
   newAccount = () => {}
 
   getAccounts = async (nStart = 0, nEnd = 5, network = 't') => {
+    this.throwIfBusy()
+    this.ledgerBusy = true
     const pathNetworkId = network === 'f' ? 1 : 461
     const paths = []
     for (let i = nStart; i < nEnd; i += 1) {
       paths.push([44, pathNetworkId, 5, 0, i])
     }
-    return mapSeries(paths, async path => {
+    const addresses = await mapSeries(paths, async path => {
       const { address } = this.handleErrors(
         await this.getAddressAndPubKey(path),
       )
       return address
     })
+    this.ledgerBusy = false
+    return addresses
   }
 
   sign = async (path, signedMessage) => {
+    this.throwIfBusy()
+    this.ledgerBusy = true
     const { signature } = this.handleErrors(
       await super.sign(path, signedMessage),
     )
+    this.ledgerBusy = false
     return signature.toString('base64')
+  }
+
+  showAddressAndPubKey = async path => {
+    this.throwIfBusy()
+    this.ledgerBusy = true
+    await super.showAddressAndPubKey(path)
+    this.ledgerBusy = false
   }
 }
 
