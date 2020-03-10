1a90857b18b82fad43822a1f8de11073052b3427
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-03-10 10:48:56 -0400

    Support array and string path fmts

--- src/utils/convertPathFmt.js
+++ src/utils/convertPathFmt.js
@@ -1,8 +1,10 @@
 // use temporarily to get path to be an array from a string format
-export default strPath =>
-  strPath
+export default strPath => {
+  if (Array.isArray(strPath)) return strPath
+  return strPath
     .slice(2)
     .split('/')
     .map(code =>
       code[code.length - 1] === "'" ? Number(code.slice(0, -1)) : Number(code),
     )
+}
