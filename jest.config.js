57f085532913ef3b239c6a9530d2bdb83e2d2792
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-08-25 17:53:33 -0400

    Upgrade to latest lotus code
    Tests use an actual node

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,5 @@ module.exports = {
   testPathIgnorePatterns: ['/node_modules/'],
   moduleNameMapper: {
     '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
-    '@openworklabs/lotus-jsonrpc-engine':
-      '<rootDir>/test/mock-lotus-jsonrpc-engine.js',
   },
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import { FilecoinNumber } from '@openworklabs/filecoin-number'
+import { FilecoinNumber, BigNumber } from '@openworklabs/filecoin-number'
 import LotusRpcEngine from '@openworklabs/lotus-jsonrpc-engine'
 import { checkAddressString } from '@openworklabs/filecoin-address'
+import { Message } from '@openworklabs/filecoin-message'
 
 export { default as LocalNodeProvider } from './providers/LocalNodeProvider'
 export * from './utils'
@@ -49,14 +50,43 @@ class Filecoin {
       if (
         err &&
         err.message &&
-        err.message.toLowerCase().includes('address not found')
+        err.message.toLowerCase().includes('actor not found')
       )
         return 0
       throw new Error(err)
     }
   }
 
-  estimateGas = async message => {
+  cloneMsgWOnChainFromAddr = async message => {
+    const clonedMsg = Object.assign({}, message)
+    try {
+      // state call errs if the from address does not exist on chain yet, lookup from actor ID to know this for sure
+      await this.jsonRpcEngine.request('StateLookupID', clonedMsg.From, null)
+    } catch (err) {
+      // if from actor doesnt exist, use a hardcoded known actor address
+      if (err.message.toLowerCase().includes('actor not found')) {
+        if (clonedMsg.From[1] === '1') clonedMessage.From = 't01'
+        if (clonedMsg.From[1] === '3') clonedMessage.From = 't01'
+        clonedMsg.From = 't01'
+      }
+    }
+    return clonedMsg
+  }
+
+  gasEstimateFeeCap = async (message = {}) => {
+    if (!message) throw new Error('No message provided.')
+    const clonedMsg = await this.cloneMsgWOnChainFromAddr(message)
+    const feeCap = await this.jsonRpcEngine.request(
+      'GasEstimateFeeCap',
+      clonedMsg,
+      0,
+      null,
+    )
+
+    return new FilecoinNumber(feeCap, 'attofil')
+  }
+
+  gasEstimateGasLimit = async message => {
     if (!message) throw new Error('No message provided.')
     const clonedMsg = Object.assign({}, message)
     try {
@@ -68,13 +98,61 @@ class Filecoin {
         clonedMsg.From = 't01'
       }
     }
-    const stateCallRes = await this.jsonRpcEngine.request(
-      'StateCall',
+    const gasLimit = await this.jsonRpcEngine.request(
+      'GasEstimateGasLimit',
       clonedMsg,
       null,
     )
-    if (stateCallRes.Error) throw new Error(stateCallRes.Error)
-    return new FilecoinNumber(stateCallRes.MsgRct.GasUsed, 'attofil')
+
+    return new FilecoinNumber(gasLimit, 'attofil')
+  }
+
+  gasEstimateGasPremium = async (message, numBlocksIncluded = 0) => {
+    if (!message) throw new Error('No message provided.')
+    const clonedMsg = await this.cloneMsgWOnChainFromAddr(message)
+
+    const gasPremium = await this.jsonRpcEngine.request(
+      'GasEstimateGasPremium',
+      numBlocksIncluded,
+      clonedMsg.From,
+      clonedMsg.GasLimit || 0,
+      null,
+    )
+
+    return new FilecoinNumber(gasPremium, 'attofil')
+  }
+
+  gasEstimateMessageGas = async (message, maxFee = '0') => {
+    if (!message) throw new Error('No message provided.')
+    const clonedMsg = await this.cloneMsgWOnChainFromAddr(message)
+    const {
+      To,
+      From,
+      Value,
+      GasPremium,
+      GasFeeCap,
+      GasLimit,
+      Method,
+      Nonce,
+      Params,
+    } = await this.jsonRpcEngine.request(
+      'GasEstimateMessageGas',
+      clonedMsg,
+      { MaxFee: maxFee },
+      null,
+    )
+
+    return new Message({
+      to: To,
+      from: From,
+      value: Value,
+      gasPremium: GasPremium,
+      gasFeeCap: GasFeeCap,
+      gasLimit: GasLimit,
+      method: Method,
+      nonce: Nonce,
+      params: Params,
+    })
   }
 }
 
